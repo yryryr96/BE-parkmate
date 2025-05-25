b6b02d25b8f6609f164dff0bd475c66317c58c66
정영록 <s9285s@naver.com>
2025-05-25 17:54:43 +0900

    feat: parking-service init

---
+++ parking-service/src/main/java/com/parkmate/parking_service/ParkingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate.parking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingServiceApplication.class, args);
+	}
+
+}
