7cd04f288442cf9e9cb0238437a1d31719c1f4ce
yryryr96 <s9285s@naver.com>
2025-05-26 11:15:37 +0900

    feat: ParkingLot 엔티티 업데이트 로직 작성

--- parking-service/src/main/java/com/parkmate/parking_service/parkinglot/application/ParkingLotServiceImpl.java
+++ parking-service/src/main/java/com/parkmate/parking_service/parkinglot/application/ParkingLotServiceImpl.java
@@ -2,6 +2,7 @@ package com.parkmate.parking_service.parkinglot.application;
 
 import com.parkmate.parking_service.common.exception.BaseException;
 import com.parkmate.parking_service.common.response.ResponseStatus;
+import com.parkmate.parking_service.parkinglot.domain.ParkingLot;
 import com.parkmate.parking_service.parkinglot.dto.request.ParkingLotCreateRequestDto;
 import com.parkmate.parking_service.parkinglot.dto.request.ParkingLotDeleteRequestDto;
 import com.parkmate.parking_service.parkinglot.dto.request.ParkingLotUpdateRequestDto;
@@ -27,12 +28,12 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     @Override
     public void update(ParkingLotUpdateRequestDto parkingLotUpdateRequestDto) {
 
-        parkingLotRepository.findByParkingLotUuidAndHostUuid(
+        ParkingLot entity = parkingLotRepository.findByParkingLotUuidAndHostUuid(
                 parkingLotUpdateRequestDto.getParkingLotUuid(),
                 parkingLotUpdateRequestDto.getHostUuid()
         ).orElseThrow(() -> new BaseException(ResponseStatus.RESOURCE_NOT_FOUND));
 
-        parkingLotRepository.save(parkingLotUpdateRequestDto.toEntity());
+        parkingLotRepository.save(createUpdatedParkingLotEntity(entity, parkingLotUpdateRequestDto));
     }
 
     @Override
@@ -43,5 +44,26 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     @Override
     public ParkingLotResponseDto findById(String parkingLotUuid) {
         return null;
+    }
+
+    private ParkingLot createUpdatedParkingLotEntity(ParkingLot entity,
+                                                     ParkingLotUpdateRequestDto parkingLotUpdateRequestDto) {
+        return ParkingLot.builder()
+                .id(entity.getId())
+                .parkingLotUuid(entity.getParkingLotUuid())
+                .hostUuid(parkingLotUpdateRequestDto.getHostUuid())
+                .name(parkingLotUpdateRequestDto.getName())
+                .phoneNumber(parkingLotUpdateRequestDto.getPhoneNumber())
+                .capacity(parkingLotUpdateRequestDto.getCapacity())
+                .registeredCapacity(parkingLotUpdateRequestDto.getRegisteredCapacity())
+                .isEvChargingAvailable(parkingLotUpdateRequestDto.getIsEvChargingAvailable())
+                .extraInfo(parkingLotUpdateRequestDto.getExtraInfo())
+                .zoneCode(entity.getZoneCode())
+                .mainAddress(entity.getMainAddress())
+                .detailAddress(entity.getDetailAddress())
+                .latitude(entity.getLatitude())
+                .longitude(entity.getLongitude())
+                .parkingLotType(entity.getParkingLotType())
+                .build();
     }
 }
