d212ab0782fd5a05b35cd09699da6358e8e21b63
yryryr96 <s9285s@naver.com>
2025-05-28 11:37:23 +0900

    feat: 충전 타입 필드 정의, enum과 매핑을 위해 새로운 테이블 생성 및 충전 타입을 enum으로 관리

--- src/main/java/com/parkmate/parkingservice/parkingspot/domain/ParkingSpot.java
+++ src/main/java/com/parkmate/parkingservice/parkingspot/domain/ParkingSpot.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,25 +22,41 @@ public class ParkingSpot {
     @Comment("주차면 이름")
     @Column(nullable = false, length = 100)
     private String name;
-    
+
     @Comment("주차장 UUID")
     @Column(nullable = false)
     private String parkingLotUuid;
-    
+
     @Enumerated(EnumType.STRING)
     @Comment("주차면 차량 타입")
     @Column(nullable = false)
     private ParkingSpotType type;
-    
+
     @Comment("전기차 충전 가능 여부")
     @Column(nullable = false)
     private Boolean isEvChargingAvailable;
 
+    @ElementCollection(targetClass = EvChargeType.class)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(
+            name = "parking_spot_charging_type",
+            joinColumns = @JoinColumn(name = "id")
+    )
+    @Column(name = "ev_charge_type")
+    private Set<EvChargeType> EvChargeTypes = new HashSet<>();
+
     @Builder
-    private ParkingSpot(String name, String parkingLotUuid, ParkingSpotType type, Boolean isEvChargingAvailable) {
+    private ParkingSpot(Long id,
+                       String name,
+                       String parkingLotUuid,
+                       ParkingSpotType type,
+                       Boolean isEvChargingAvailable,
+                       Set<EvChargeType> evChargeTypes) {
+        this.id = id;
         this.name = name;
         this.parkingLotUuid = parkingLotUuid;
         this.type = type;
         this.isEvChargingAvailable = isEvChargingAvailable;
+        EvChargeTypes = evChargeTypes;
     }
 }
