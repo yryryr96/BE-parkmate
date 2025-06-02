a7872dca1c40af9022ef4d8b64bc1f3be9944859
정영록 <s9285s@naver.com>
2025-06-02 23:48:44 +0900

    fix: parkingLotUuid로 hostUuid를 조회하는 API 분리

--- src/main/java/com/parkmate/parkingservice/parkinglot/application/ParkingLotServiceImpl.java
+++ src/main/java/com/parkmate/parkingservice/parkinglot/application/ParkingLotServiceImpl.java
@@ -6,6 +6,7 @@ import com.parkmate.parkingservice.parkinglot.domain.ParkingLot;
 import com.parkmate.parkingservice.parkinglot.dto.request.ParkingLotRegisterRequestDto;
 import com.parkmate.parkingservice.parkinglot.dto.request.ParkingLotDeleteRequestDto;
 import com.parkmate.parkingservice.parkinglot.dto.request.ParkingLotUpdateRequestDto;
+import com.parkmate.parkingservice.parkinglot.dto.response.ParkingLotHostUuidResponseDto;
 import com.parkmate.parkingservice.parkinglot.dto.response.ParkingLotResponseDto;
 import com.parkmate.parkingservice.parkinglot.infrastructure.ParkingLotRepository;
 import lombok.RequiredArgsConstructor;
@@ -54,6 +55,12 @@ public class ParkingLotServiceImpl implements ParkingLotService {
                 parkingLotRepository.findByParkingLotUuid(parkingLotUuid)
                         .orElseThrow(() -> new BaseException(ResponseStatus.RESOURCE_NOT_FOUND))
         );
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public ParkingLotHostUuidResponseDto getHostUuidByParkingLotUuid(String parkingLotUuid) {
+        return ParkingLotHostUuidResponseDto.from(parkingLotRepository.findHostUuidByParkingLotUuid(parkingLotUuid));
     }
 
     private ParkingLot createUpdatedParkingLotEntity(ParkingLot entity,
