8000109b1b171d052f94a4d1f7bfbffbcd58672a
yryryr96 <s9285s@naver.com>
2025-06-05 11:54:01 +0900

    init: init project

---
+++ src/main/java/com/parkmate/parkingreadservice/ParkingreadserviceApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate.parkingreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingreadserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingreadserviceApplication.class, args);
+	}
+
+}
