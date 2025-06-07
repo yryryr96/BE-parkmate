2b40919723e08ddc532c43cc47951dfc0bf00868
정영록 <s9285s@naver.com>
2025-06-08 03:03:59 +0900

    fix: 테스트 컨트롤러 삭제

--- src/main/java/com/parkmate/parkingreadservice/parkinglotread/presentation/ParkingLotReadController.java
+++ src/main/java/com/parkmate/parkingreadservice/parkinglotread/presentation/ParkingLotReadController.java
@@ -2,11 +2,13 @@ package com.parkmate.parkingreadservice.parkinglotread.presentation;
 
 import com.parkmate.parkingreadservice.common.response.ApiResponse;
 import com.parkmate.parkingreadservice.parkinglotread.application.ParkingLotReadService;
-import com.parkmate.parkingreadservice.parkinglotread.event.ParkingLotMetadataUpdateEvent;
 import com.parkmate.parkingreadservice.parkinglotread.vo.response.ParkingLotReadResponseVo;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/parking-lot-read")
@@ -28,12 +30,4 @@ public class ParkingLotReadController {
                 parkingLotReadService.getParkingLotReadByParkingLotUuid(parkingLotUuid).toVo()
         );
     }
-
-    @PutMapping("/{parkingLotUuid}")
-    public ApiResponse<String> updateParkingLotMetadata(@PathVariable String parkingLotUuid,
-                                                        @RequestBody ParkingLotMetadataUpdateEvent parkingLotMetadataUpdateEvent) {
-        parkingLotReadService.syncParkingLotMetadata(parkingLotMetadataUpdateEvent);
-        return ApiResponse.ok("주차장 메타데이터 업데이트에 성공했습니다.");
-    }
-
 }
