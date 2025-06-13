9cd09b04ca9e9ca4e98759f63f9efc74110c475e
정영록 <s9285s@naver.com>
2025-06-14 03:35:11 +0900

    feat: reservation 서버와 통신해서 필요한 주차면 정보 제공 client api 생성

--- src/main/java/com/parkmate/parkingservice/parkingspot/presentation/InternalParkingSpotController.java
+++ src/main/java/com/parkmate/parkingservice/parkingspot/presentation/InternalParkingSpotController.java
@@ -3,6 +3,7 @@ package com.parkmate.parkingservice.parkingspot.presentation;
 import com.parkmate.parkingservice.facade.parkingspot.ParkingSpotFacade;
 import com.parkmate.parkingservice.parkingspot.dto.request.ParkingSpotClientRequest;
 import com.parkmate.parkingservice.parkingspot.dto.response.ParkingSpotClientResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,12 @@ public class InternalParkingSpotController {
 
     private final ParkingSpotFacade parkingSpotFacade;
 
+    @Operation(
+            summary = "RESERVATION-SERVICE에서 주차 공간 정보 조회",
+            description = "주차 공간 정보를 조회하는 API입니다. " +
+                          "주차장 UUID와 주차 공간 타입을 기반으로 주차 공간 정보를 반환합니다.",
+            tags = {"PARKING-SPOT-SERVICE"}
+    )
     @PostMapping("/search")
     public ParkingSpotClientResponse getParkingLotsSimpleInfo(@RequestBody ParkingSpotClientRequest parkingSpotClientRequest) {
         return parkingSpotFacade.getParkingSpotsByParkingLotUuidAndType(parkingSpotClientRequest);
