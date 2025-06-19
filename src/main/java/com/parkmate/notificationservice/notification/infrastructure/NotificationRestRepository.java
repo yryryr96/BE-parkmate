891e0fd47863c1d7b20c50ee64e4100363f4f290
yryryr96 <s9285s@naver.com>
2025-06-19 17:56:04 +0900

    feat: web push api 추가

--- src/main/java/com/parkmate/notificationservice/notification/infrastructure/NotificationRestRepository.java
+++ src/main/java/com/parkmate/notificationservice/notification/infrastructure/NotificationRestRepository.java
@@ -1,10 +1,8 @@
 package com.parkmate.notificationservice.notification.infrastructure;
 
 import com.parkmate.notificationservice.notification.domain.Notification;
-import com.parkmate.notificationservice.notification.dto.response.NotificationResponseDto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface NotificationRestRepository extends MongoRepository<Notification, String> {
