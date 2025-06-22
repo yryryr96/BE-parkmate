b91ffb63e148c1f4756ff6a7bd7b7a1784bde58c
정영록 <s9285s@naver.com>
2025-06-22 23:02:05 +0900

    chore: sql파일 삭제 및 discoveryclient 설정

--- src/main/java/com/parkmate/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/parkmate/notificationservice/NotificationServiceApplication.java
@@ -2,10 +2,11 @@ package com.parkmate.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 
