f24e68c86434af1602e07a1259e909487455d829
yryryr96 <s9285s@naver.com>
2025-06-25 11:06:21 +0900

    init: initialize project

---
+++ src/main/java/com/parkmate/userparkinghistoryservice/UserparkinghistoryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate.userparkinghistoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserparkinghistoryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserparkinghistoryserviceApplication.class, args);
+	}
+
+}
