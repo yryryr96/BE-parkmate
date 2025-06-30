39446ba78d51ac5fb8ae9d9961d9742bef932120
yryryr96 <s9285s@naver.com>
2025-06-30 14:54:39 +0900

    init project

---
+++ src/main/java/com/parkmate/parkmateorderservice/ParkmateorderserviceApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate.parkmateorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkmateorderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkmateorderserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkmate/parkmateorderservice/ParkmateorderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkmate.parkmateorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkmateorderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
